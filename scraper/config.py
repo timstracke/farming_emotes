8c96322d30dff8ed1ebd5d8d6cbc50878057b1a4
Tim Stracke <timstracke2609@gmail.com>
2021-08-26 18:02:19 +0200

    added streamers page

--- scraper/config.py
+++ scraper/config.py
@@ -3,7 +3,7 @@ EMOTES = ["LULW", "OMEGALUL", "LUL", "Pog", "PogU", "PogChamp", "PogO", "Pepega"
 STREAMERS = ["mizkif", "ludwig", "xqcow", "nmplol", "esfandtv", "mayahiga", "atrioc", "erobb221", "hasanabi", "willneff", "sodapoppin", "alinity", "qtcinderella", "trainwreckstv", "russel", "simply", "austinshow", "greekgodx", "asmongold", "cyr"]
 
 
-OAUTH_TOKEN = "oauth:g03ilnkwsvyefm8mhyarkn8oc1klrw"
-NICKNAME = "multitiminho2609"
+OAUTH_TOKEN = "oauth token"
+NICKNAME = "username"
 
 DEBUG = True
